ff4eec505b3b28c7c48395d45a9986a570511da8
rayyanabubakr <71322861+rayyanabubakr@users.noreply.github.com>
2021-12-02 16:13:16 +0530

    Create 21-MergeTwoSortedLists.py

---
+++ 21-MergeTwoSortedLists.py
@@ -0,0 +1,28 @@
+#You are given the heads of two sorted linked lists list1 and list2.
+#Merge the two lists in a one sorted list. The list should be made by splicing together the nodes of the first two lists.
+#Return the head of the merged linked list.
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+class Solution:
+    def mergeTwoLists(self, l1: ListNode, l2: ListNode) -> ListNode:
+        head = ListNode()
+        pointer = head
+        while l1 != None and l2 != None:
+            if l1.val < l2.val:
+                pointer.next = l1
+                l1 = l1.next
+            else:
+                pointer.next = l2
+                l2 = l2.next
+            pointer = pointer.next
+        if l1 != None:
+            pointer.next = l1
+        if l2 != None:
+            pointer.next = l2
+        return head.next
+                
+        
+        
